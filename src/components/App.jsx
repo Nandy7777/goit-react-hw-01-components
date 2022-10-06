c42eb25d3f237f06885dfce1af44d3e0afd3f243
Nandy7777 <nandy7777@ukr.net>
2022-10-06 21:11:18 +0300

    перше розмітка + початок другого

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import Profile from '../components/Profile/profile';
+import user from 'components/user.json';
+import data from 'components/data.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </div>
+
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101'
+    //   }}
+    // >
+    //   React homework template
+    // </div>
   );
 };
